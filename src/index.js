618f24f8c4eb5c2d66ece9da4c3a19e677dd8e38
Paul Siegrist <paul.siegrist@mediaman.de>
2019-12-17 14:08:20 +0100

    formatting

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const text = 'text hallo 123';
 
 const template = require('./components/testing/otherpartial.hbs');
-document.getElementById('test').innerHTML = template({ text });
+document.getElementById('test').innerHTML = template({text});
